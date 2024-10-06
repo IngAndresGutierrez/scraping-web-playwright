a0c680c8975648382ce243af9e3d27429bc5f37a
Jaime Andrés Gómez Gutiérrez <g.gutierrez.j.andres@gmail.com>
2024-10-06 15:06:28 +0200

    feat: demo scraping to amazon page was added

---
+++ index.mjs
@@ -0,0 +1,26 @@
+import { chromium } from "playwright"
+
+const browser = await chromium.launch({ headless: true })
+const page = await browser.newPage()
+
+await page.goto(
+  "https://www.amazon.es/s?k=play+starion+5&crid=3R3209YUGOOA1&sprefix=play+s%2Caps%2C106&ref=nb_sb_ss_pltr-mrr_1_6"
+)
+
+const products = await page.$$eval(".s-card-container", (results) =>
+  results.map((el) => {
+    const title = el.querySelector("h2")?.innerText
+
+    if (!title) return null
+
+    const image = el.querySelector("img").getAttribute("src")
+    const price = el.querySelector(".a-price .a-offscreen")?.innerText
+    const link = el.querySelector(".a-link-normal").getAttribute("href")
+
+    return { title, image, price, link }
+  })
+)
+
+console.log(products)
+
+await browser.close()
